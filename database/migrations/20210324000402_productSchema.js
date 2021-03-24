843da50f86a78c33bba810e3f03e4dddbd04ade4
dojo24 <mlongenberger@codingdojo.com>
2021-03-23 20:43:00 -0400

    changed to string

--- database/migrations/20210324000402_productSchema.js
+++ database/migrations/20210324000402_productSchema.js
@@ -5,7 +5,7 @@ exports.up = function(knex) {
   .createTable('products', tbl => {
       tbl.increments();
       tbl.string('title').unique().notNullable();
-      tbl.integer('price');
+      tbl.string('price');
       tbl.string('description');
       tbl.string('category');
       tbl.string('image');
